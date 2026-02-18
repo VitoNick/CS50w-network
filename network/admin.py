9efe805ec7cb72d494f34f00dbadf81686a6a65e
Matt Nicassio <vitonick81@gmail.com>
2026-02-18 10:39:42 -0800

    Environment setup, TODO models added. Pushing before migrations, and before adding New Post. Going to experiment with a new post modal window.

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'date_joined']
+    search_fields = ['username', 'email']
+
+
+admin.site.register(User, UserAdmin)
--- network/models.py
+++ network/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    # TODO: 
+    pass
+
+class Like(models.Model):
+    # TODO:
+    pass
+
+class Follower(models.Model):
+    # TODO:
+    pass
